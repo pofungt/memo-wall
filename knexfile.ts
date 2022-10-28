3c97d70957721916da688fb6bc16d64915342c61
Duncan Tang <pofungt@hotmail.com>
2022-10-28 09:38:31 -0400

    Try Github Workflow

--- knexfile.ts
+++ knexfile.ts
@@ -26,9 +26,10 @@ const config: { [key: string]: Knex.Config } = {
     test:{
         client: 'postgresql',
         connection: {
-            database: process.env.TESTDB_NAME,
-            user:     process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD
+			host: process.env.POSTGRES_HOST ,
+			database: process.env.POSTGRES_DB,
+			user:     process.env.POSTGRES_USER,
+			password: process.env.POSTGRES_PASSWORD
         },
         pool: {
             min: 2,
@@ -58,9 +59,9 @@ const config: { [key: string]: Knex.Config } = {
 	production: {
 		client: 'postgresql',
 		connection: {
-			database: 'my_db',
-			user: 'username',
-			password: 'password'
+			database: process.env.DB_NAME,
+			user:     process.env.DB_USERNAME,
+			password: process.env.DB_PASSWORD
 		},
 		pool: {
 			min: 2,
